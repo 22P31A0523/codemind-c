5d9d130e16a512e8608d9ac8a63d18ba51c63019
22P31A0523 <119730473+22P31A0523@users.noreply.github.com>
2023-06-02 16:34:16 +0530

    #include<stdio.h> int main() {     int n,m,q,c=0,tc=0,r,i;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         q=i;         c=0;         tc=0;         while(q!=0)         {             c=c+i;             r=q%10;             if(r!=0&&i%r==0)             tc=tc+i;             q=q/10;         }         if(c==tc)         printf("%d ",i);     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int l,n,i,q,r,c=0,tc=0;
-    scanf("%d%d",&l,&n);
-    for(i=l;i<=n;i++)
+    int n,m,q,c=0,tc=0,r,i;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
     {
         q=i;
         c=0;
         tc=0;
         while(q!=0)
         {
-            c=c+1;
+            c=c+i;
             r=q%10;
             if(r!=0&&i%r==0)
-            {
-                tc=tc+1;
-            }
+            tc=tc+i;
             q=q/10;
         }
         if(c==tc)
-        {
-            printf("%d ",i);
-        }
+        printf("%d ",i);
     }
 }
